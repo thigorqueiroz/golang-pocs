746ca409995bee201d8dd8c8a1d4726912c94353
Thiago <thiago.q-silva@sefaz.pe.gov.br>
2020-07-28 18:10:06 -0300

    adding net/http

--- web-app-in-go/main.go
+++ web-app-in-go/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 const extension = ".txt"
@@ -15,6 +17,14 @@ type page struct {
 func (p *page) save() error {
 	filename := p.Title + extension
 	return ioutil.WriteFile(filename, p.Body, 0600) //WriteFile is a standard library function that writes a byte slice to a file
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("debug Thiago " + r.URL.Path)
+
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1<div>%s</div>", p.Title, p.Body)
 }
 
 func loadPage(title string) (*page, error) {
@@ -34,4 +44,7 @@ func main() {
 
 	p2, _ := loadPage("testPage")
 	fmt.Println(string(p2.Body))
+
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
